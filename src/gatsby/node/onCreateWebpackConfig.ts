869a3c43c3022139622d74ab438a76558ed45ffb
woo3145 <lcwoo3145@gmail.com>
2023-11-30 12:29:34 +0900

    Fix - WebpackConfig 누락 (@/hooks alias)

--- src/gatsby/node/onCreateWebpackConfig.ts
+++ src/gatsby/node/onCreateWebpackConfig.ts
@@ -9,6 +9,7 @@ const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
       alias: {
         '@/components': path.resolve(__dirname, '../../components'),
         '@/lib/utils': path.resolve(__dirname, '../../lib/utils'),
+        '@/hooks': path.resolve(__dirname, '../../hooks'),
       },
     },
   });
