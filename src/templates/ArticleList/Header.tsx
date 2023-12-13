ecb2ee2dd7b84c2f64120d3d13564898adeb3fcf
woo3145 <lcwoo3145@gmail.com>
2023-12-13 18:07:18 +0900

    게시물 필터기능 마무리

--- src/templates/ArticleList/Header.tsx
+++ src/templates/ArticleList/Header.tsx
@@ -14,20 +14,25 @@ export const ArticleListHeader = ({ filter, totalPost }: Props) => {
   return (
     <div className="w-full">
       <div className="w-full flex flex-col justify-center items-center space-y-3 py-12">
-        <h3 className="text-5xl font-semibold"># {filter}</h3>
+        <h3 className="text-5xl font-semibold"># {filter ? filter : 'All'}</h3>
         <p className="text-foreground/60 text-lg">posts {totalPost}</p>
       </div>
 
       <div className="flex flex-wrap gap-4">
         {categories.map((category) => {
+          const isSeleced = category === filter;
           return (
             <Link
               key={category}
-              to={`${location.pathname.slice(0, -1)}?filter=${category}`}
+              to={
+                isSeleced
+                  ? `${location.pathname}?filter=`
+                  : `${location.pathname}?filter=${category}`
+              }
             >
               <Button
                 className="uppercase"
-                variant={category === filter ? 'default' : 'outline'}
+                variant={isSeleced ? 'default' : 'outline'}
               >
                 {category}
               </Button>
